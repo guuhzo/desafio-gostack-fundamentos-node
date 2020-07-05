9f4fc7bdea8e37411c8dcf9ae8161433ae6810dd
guuh017 <gs-nascimento@hotmail.com>
2020-07-04 23:42:51 -0300

    Add project

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('the transaction value is greater than the income');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('the transaction type is not available');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactionsRepository.create(transaction);
+
+    return transaction;
   }
 }
 
